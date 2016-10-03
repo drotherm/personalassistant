9bd203933f24d6f740fcc640c839a721759829e7
Dede Danielle Rothermel <drotherm@drotherm-mbp.local>
2016-10-02 23:12:37 -0400

    Initial Commit

---
+++ PersonalAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonalAssistant
+//
+//  Created by Dede Danielle Rothermel on 10/2/16.
+//  Copyright © 2016 Danielle Rothermel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
